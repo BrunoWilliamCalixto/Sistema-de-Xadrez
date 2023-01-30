a006ecf0034248e8342b0fbacc1f3450675904dd
Bruno William <brunowilliamcalixto@gmail.com>
2023-01-30 15:31:07 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces; // declaração da matriz de peças
 
     public Board(int rows, int columns) {
+        if(rows < 1 || columns <1){
+            throw new BoardException("Erro ao criando tabuleiro: é necessário que exista 1 linha e uma coluna");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -15,28 +18,44 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece piece(int row, int column) {
+        if(!positionExists(row, column)){
+            throw new BoardException("Posição não se encontra no tabuleiro");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
+        if(!positionExists(position)){
+            throw new BoardException("Posição não se encontra no tabuleiro");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position){ //passado como argumento no método, peça e posição
+        if(thereIsAPiece(position)){
+            throw new BoardException("Peça já está na posição " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece; // pegamos a matriz na posição dada, e atribuí a peça que informei
         piece.position = position; //Atualização de peça através da classe Piece, consigo então, acessar livremente a posição da peça
+    }
+
+    private boolean positionExists(int row, int column){ // posição dentro do tabuleiro
+       return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position){ // sobrecarga de método, onde se é retornado a posição de linha e coluna do tabuleiro
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position){ // se a peça do tabuleiro for diferente de nulo, ela retorna verdadeiro e retona a peça para a matriz de Position
+        if(!positionExists(position)){
+            throw new BoardException("Posição não se encontra no tabuleiro");
+        }
+        return piece(position) != null;
     }
 }
